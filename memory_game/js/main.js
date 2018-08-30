c727e066df69ea2294eb6123887dce0b422c3f30
Emily L <embem80@gmail.com>
2018-08-30 16:00:34 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,6 @@
 //console.log("Up and running!");
 
+//cards array with rank, suit, image face up
 var cards = [
 {
 	rank: "queen",
@@ -23,28 +24,47 @@ var cards = [
 }
 ];
 
+//cards in play array. cards (rank - king/queen) added here by flipCard function
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		//console.log("You found a match!");
+	//check if flipped cards match eachother and show alert
+	if (cardsInPlay[0] === cardsInPlay[1] || cardsInPlay[0] === cardsInPlay[2] || cardsInPlay[0] === cardsInPlay[3]) {
+		console.log(cardsInPlay);
 		alert("You found a match!");
+		//location.reload();
 	} else {
-		//console.log("Sorry, try again.");
 		alert("Sorry, try again.");
 	}
 };
 
-var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
+//function called when user clicks on a card
+var flipCard = function() {
+	//assign the data id (array index) to card flipped and save as cardId
+	var cardId = this.getAttribute('data-id');
+	//add rank (king/queen) of card flipped to the cardsinplay array
 	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();	
-	console.log(cards[cardId].cardImage);
-    console.log(cards[cardId].suit);
+	//run this function to check for matching flipped cards
+	//change image source for card flipped, get image src from cards array
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length >= 2) {
+		checkForMatch();
+	} else {
+		alert("flip another card");
+	};
 };
 
-flipCard(0);
-flipCard(2);
+//first function called - creates board with backs of cards, assigns data-id (index number) to each card in cards array
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	};
+};
 
+createBoard();
 
 
